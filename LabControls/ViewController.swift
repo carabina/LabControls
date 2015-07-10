6a0e52a631e3fc1d120f650b3be935c55b733ae6
Ruben Nine <ruben@9labs.io>
2015-07-10 19:18:03 +0200

    Initial commit

---
+++ LabControls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LabControls
+//
+//  Created by Ruben on 7/10/15.
+//  Copyright © 2015 9Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
