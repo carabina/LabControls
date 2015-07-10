38ff113f2eda5002dd3386f38ba4aae0accded82
Ruben Nine <ruben@9labs.io>
2015-07-10 21:04:07 +0200

    Adding some controls from code using PureLayout for autolayout.

--- LabControls/ViewController.swift
+++ LabControls/ViewController.swift
@@ -7,32 +7,77 @@
 //
 
 import Cocoa
+import PureLayout
 
 class ViewController: NSViewController {
 
-    var mainWindow: MainWindow?
+    weak var mainWindow: MainWindow?
+    private var didSetupConstraints = false
+
+    private lazy var okButton: NSButton = {
+        let obj = NSButton(forAutoLayout: ())
+
+        obj.bezelStyle = .RoundedBezelStyle
+        obj.title = "Ok"
+        obj.keyEquivalent = "\r"
+
+        return obj
+    }()
+
+    private lazy var cancelButton: NSButton = {
+        let obj = NSButton(forAutoLayout: ())
+
+        obj.bezelStyle = .RoundedBezelStyle
+        obj.title = "Cancel"
+
+        return obj
+    }()
+
+    private lazy var launchToggleButton: NSButton = {
+        let obj = NSButton(forAutoLayout: ())
+
+        obj.setButtonType(.SwitchButton)
+        obj.title = "Launch at startup"
+
+        return obj
+    }()
+
+    private lazy var popupButton: NSPopUpButton = {
+        let obj = NSPopUpButton(forAutoLayout: ())
+
+        obj.menu?.removeAllItems()
+
+        for item in ["One", "Two", "Three"] {
+            obj.menu?.addItemWithTitle(item, action: nil, keyEquivalent: "")
+        }
+
+        return obj
+    }()
+
+    private lazy var textField: NSTextField = {
+        let obj = NSTextField(forAutoLayout: ())
+
+        obj.stringValue = "Some value goes here!"
+
+        return obj
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-
-        NSNotificationCenter.defaultCenter().addObserver(self,
-            selector: "toggleDarkUI:",
-            name: AppNotification.ToggleDarkUI,
-            object: nil
-        )
+        addControls()
+        updateViewConstraints()
+        registerForNotifications()
     }
 
     deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        unregisterForNotifications()
     }
 
     override func viewWillAppear() {
         if let mainWindow = view.window as? MainWindow {
             self.mainWindow = mainWindow
-            mainWindow.title = "UI / UX Lab"
-            mainWindow.setVibrantLightAppearance()
+            resetMainWindow()
         }
     }
 
@@ -40,6 +85,59 @@ class ViewController: NSViewController {
         didSet {
             // Update the view, if already loaded.
         }
+    }
+
+    private func resetMainWindow() {
+        mainWindow?.title = "UI / UX Lab"
+        mainWindow?.setVibrantLightAppearance()
+    }
+
+    private func addControls() {
+        view.addSubview(textField)
+        view.addSubview(okButton)
+        view.addSubview(cancelButton)
+        view.addSubview(launchToggleButton)
+        view.addSubview(popupButton)
+    }
+
+    override func updateViewConstraints() {
+        if didSetupConstraints == false {
+            didSetupConstraints = true
+
+            // Setup constraints
+            view.autoSetDimension(.Width, toSize: 400, relation: .GreaterThanOrEqual)
+            view.autoSetDimension(.Height, toSize: 240, relation: .GreaterThanOrEqual)
+
+            okButton.autoPinEdgeToSuperviewEdge(.Right, withInset: 22)
+            okButton.autoPinEdgeToSuperviewEdge(.Bottom, withInset: 22)
+
+            cancelButton.autoPinEdge(.Right, toEdge: .Left, ofView: okButton, withOffset: -11)
+            cancelButton.autoPinEdgeToSuperviewEdge(.Bottom, withInset: 22)
+
+            launchToggleButton.autoPinEdgeToSuperviewEdge(.Left, withInset: 22)
+            launchToggleButton.autoPinEdgeToSuperviewEdge(.Bottom, withInset: 22)
+
+            popupButton.autoPinEdgeToSuperviewEdge(.Right, withInset: 22)
+            popupButton.autoPinEdge(.Bottom, toEdge: .Top, ofView: okButton, withOffset: -11)
+
+            textField.autoPinEdgeToSuperviewEdge(.Top, withInset: 22)
+            textField.autoPinEdgeToSuperviewEdge(.Left, withInset: 22)
+            textField.autoPinEdgeToSuperviewEdge(.Right, withInset: 22)
+        }
+
+        super.updateViewConstraints()
+    }
+
+    private func registerForNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "toggleDarkUI:",
+            name: AppNotification.ToggleDarkUI,
+            object: nil
+        )
+    }
+
+    private func unregisterForNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     @objc private func toggleDarkUI(notification: NSNotification) {
@@ -52,4 +150,3 @@ class ViewController: NSViewController {
         }
     }
 }
-
